adaf61454e277b55689debe435fded8716c27fb0
GlauberC <glaubercarv@gmail.com>
2022-08-19 12:06:39 -0300

    add

---
+++ 02-iniciando-api/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Test" });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
